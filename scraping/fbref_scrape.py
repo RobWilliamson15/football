7c223b98f3b06502783501f3b7dbc101a7ab8275
Rob <rwilliamson15@qub.ac.uk>
2023-10-20 13:05:37 +0100

    Address some pylint issues

--- scraping/fbref_scrape.py
+++ scraping/fbref_scrape.py
@@ -1,6 +1,9 @@
-import requests
+"""
+Scrape fbref for premier league fixtures and results
+"""
 import csv
 from datetime import datetime
+import requests
 from bs4 import BeautifulSoup
 
 # Define a list of seasons to loop through
@@ -11,14 +14,14 @@ seasons = range(2020, 2023)
 all_data = []
 
 for season in seasons:
-    
-    # Get season start and end 
+
+    # Get season start and end
     season_start = season
-    season_end = season + 1 
-    
+    season_end = season + 1
+
     # Create standings_url
     standings_url =f"https://fbref.com/en/comps/9/{season_start}-{season_end}/schedule/{season_start}-{season_end}-Premier-League-Scores-and-Fixtures"
-    
+
     # Get data
     data = requests.get(standings_url)
     soup = BeautifulSoup(data.text, features='lxml')
@@ -31,39 +34,39 @@ for season in seasons:
         for i in rows:
             # round and day
             round = i.find('th',{'data-stat':'gameweek'})
-            if round!=None:
+            if round is not None:
                 Round = round.text
             day = i.find('td',{'data-stat':'dayofweek'})
-            if day!= None:
+            if day is not None:
                 Day=day.text
 
 
             #date
             date=i.find('td',{'data-stat':'date'})
             date = date.find('a')
-            if date!=None:
+            if date is not None:
                 Date=date.text
                 #converting date formats
                 date_obj = datetime.strptime(Date, "%Y-%m-%d")
                 Date = date_obj.strftime("%b %d %Y")
-      
+
 
             #home_team
             ht= i.find('td',{'data-stat':'home_team'})
             ht=ht.find('a')
-            if ht !=None:
+            if ht is not None:
                 Ht=ht.text
-    
+
             #score
             sc= i.find('td',{'data-stat':'score'})
             sc=sc.find('a')
-            if sc!=None:
+            if sc is not None:
                 Sc=sc.text
-    
+
             #away_team
             at= i.find('td',{'data-stat':'away_team'})
             at=at.find('a')
-            if at!=None:
+            if at is not None:
                 At=at.text
 
             #each game
@@ -72,11 +75,11 @@ for season in seasons:
             game = Round + ',' + Day + ' ' + Date + ',' +Ht + ',' + Sc + ',' + At
             main_list.append(game)
         main_list.sort(key = lambda x: int(x.split(',')[0]))
-        
+
         # Append the data for this season to the overall data list
         all_data.extend(main_list)
-  
-with open('../data/fixture_results.csv', 'w', newline='') as file:
+
+with open('../data/fixture_results.csv', 'w', newline='', encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["Round", "Date", "Team 1", "FT", "Team 2"])
     for row in all_data:
--- test.py
+++ test.py
@@ -1,13 +1,19 @@
+"""
+Trains and tests a model based on the scraped data from fbref
+"""
 import pandas as pd
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn
+from torch import optim
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report
 
 def data_prep(file_path):
+    """
+    Preps the data for training and testing
+    """
     # Load the CSV data into a Pandas DataFrame
     data = pd.read_csv(file_path)
 
@@ -23,7 +29,8 @@ def data_prep(file_path):
     data[['home_score', 'away_score']] = data['FT'].str.split('-', expand=True).astype(int)
 
     # Create a target variable (match outcome) as numerical labels (0 for Win, 1 for Draw, 2 for Lose)
-    data['outcome'] = np.where(data['home_score'] > data['away_score'], 0, np.where(data['home_score'] == data['away_score'], 1, 2))
+    data['outcome'] = np.where(data['home_score'] > data['away_score'], 0,
+                              np.where(data['home_score'] == data['away_score'], 1, 2))
 
     # Define features and target
     X = data[['Team1', 'Team2']].values
@@ -35,18 +42,24 @@ def data_prep(file_path):
 
 # Define a simple feedforward neural network using PyTorch
 class Net(nn.Module):
+    """
+    Defines a simple feedforward neural net
+    """
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(2, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 3)  # 3 output classes (Win, Draw, Lose)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
 def train(X_train, X_test, y_train, y_test):
+    """
+    Trains and tests the Net model with the prepped data
+    """
     # Initialize the model, loss function, and optimizer
     model = Net()
     criterion = nn.CrossEntropyLoss()
@@ -54,7 +67,7 @@ def train(X_train, X_test, y_train, y_test):
 
     # Training the model
     num_epochs = 100
-    for epoch in range(num_epochs):
+    for _ in range(num_epochs):
         inputs = torch.tensor(X_train, dtype=torch.float32)
         labels = torch.tensor(y_train, dtype=torch.long)
 
@@ -77,5 +90,6 @@ def train(X_train, X_test, y_train, y_test):
     print("Accuracy:", accuracy)
     print("Classification Report:\n", report)
 
-X_train, X_test, y_train, y_test = data_prep('data/cleaned_fixture_results.csv')
-train(X_train, X_test, y_train, y_test)
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = data_prep('data/cleaned_fixture_results.csv')
+    train(X_train, X_test, y_train, y_test)
